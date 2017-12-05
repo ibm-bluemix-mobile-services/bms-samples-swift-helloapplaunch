31cf7ca56511ecd573883b241dd5440472093773
Vittal Pai <vittalpai@ymail.com>
2017-12-05 10:59:49 +0530

    Update sample to make it work with AppLaunch pod framework

--- Engage-Pizza/Pods/IBMAppLaunch/AppLaunch/AppLaunch/AppLaunch.swift
+++ Engage-Pizza/Pods/IBMAppLaunch/AppLaunch/AppLaunch/AppLaunch.swift
@@ -198,12 +198,16 @@ public func actions(completionHandler:@escaping(_ features:JSON?, _ statusCode:I
                 
                 if(status == 200 || status == 201){
                     if let data = responseText.data(using: String.Encoding.utf8) {
-                        let respJson = JSON(data: data)
+                        do {
+                        let respJson = try JSON(data: data)
                         
                         print("response data from server \(responseText)")
                         self.features = respJson["features"];
                         completionHandler(respJson["features"],200,"")
-                    }
+                        }catch{
+                            print("Error: "+error.localizedDescription)
+                            completionHandler(nil,nil,error.localizedDescription)
+                        }}
                 }else{
                     print("[404] Actions Not found")
                     completionHandler(nil,status,responseText)
